724523da04a0792f4668f67efcaa4b26f5948fc9
Caio Magno <97899694+CaioDevJava@users.noreply.github.com>
2022-05-22 03:33:08 -0300

    Update ModuloConexao.java

--- src/br/com/conexao/ModuloConexao.java
+++ src/br/com/conexao/ModuloConexao.java
@@ -26,7 +26,7 @@ public class ModuloConexao {
         // Armazenando informações referentes ao banco
         final String url = "jdbc:mysql://localhost:3306/DOARPI";
         String user = "root";
-        String password = "Cm#210690";
+        String password = "";
         // Estabelecendo a Conexão com o banco
         try {
             Class.forName(driver);
